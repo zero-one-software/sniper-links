ae63575ddc74a2b2eb95427123d374c011dee173
Gabriel Fortuna <gabriel@zero-one.io>
2025-05-23 11:17:07 +0200

    refactor: simplify tests

--- spec/sniper_links_spec.rb
+++ spec/sniper_links_spec.rb
@@ -5,14 +5,35 @@ RSpec.describe SniperLinks do
     expect(SniperLinks::VERSION).not_to be nil
   end
 
+  subject(:sniper_link_obj) { SniperLinks.new(email) }
+
+  let(:from) { "howdy@zero-one.io" }
+
   context "when working with GMail" do
     context "when supplied an email ending in @gmail.com" do
-      subject { SniperLinks.new(email) }
-
       let(:email) { "plop@gmail.com" }
 
       it "is expected to use the GMail sniper link strategy automatically" do
-        expect(subject.sniper_link_strategy).to be_a(SniperLinks::Strategies::GMail)
+        expect(sniper_link_obj.sniper_link_strategy).to be_a(SniperLinks::Strategies::GMail)
+      end
+
+      context "when generating a sniper link" do
+        subject(:strategy_obj) { sniper_link_obj.sniper_link(from) }
+
+        it "is expected to generate a sniper link" do
+          expect(strategy_obj).to be_a(URI)
+        end
+
+        it "is expected to point to the correct URL" do
+          expect(strategy_obj.host).to eq "mail.google.com"
+        end
+
+        it "is expected to have sniper params" do
+          expect(strategy_obj.fragment).to start_with("search/")
+        end
+      end
+    end
+  end
       end
 
       context "when generating a sniper link" do
