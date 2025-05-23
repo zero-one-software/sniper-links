b93e23b73449b0d5db09dbadfe94ea139c22247e
Gabriel Fortuna <gabriel@zero-one.io>
2025-05-23 11:16:49 +0200

    conf: add rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,11 @@ require "sniper_links"
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
+  config.profile_examples                     = 3
+  config.order                                = :random
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
