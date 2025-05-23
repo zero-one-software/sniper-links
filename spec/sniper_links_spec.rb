2aab25b295c986796fcbfe5759cfab19a4dd0e2c
Gabriel Fortuna <gabriel@zero-one.io>
2025-05-23 12:44:45 +0200

    refactor: use rspec-its for cleaner tests

--- spec/sniper_links_spec.rb
+++ spec/sniper_links_spec.rb
@@ -13,24 +13,15 @@ RSpec.describe SniperLinks do
     context "when supplied an email ending in @gmail.com" do
       let(:email) { "plop@gmail.com" }
 
-      it "is expected to use the GMail sniper link strategy automatically" do
-        expect(sniper_link_obj.sniper_link_strategy).to be_a(SniperLinks::Strategies::GMail)
-      end
+      its(:sniper_link_strategy) { is_expected.to be_a(SniperLinks::Strategies::GMail) }
 
       context "when generating a sniper link" do
         subject(:strategy_obj) { sniper_link_obj.sniper_link(from) }
 
-        it "is expected to generate a sniper link" do
-          expect(strategy_obj).to be_a(URI)
-        end
+        it { is_expected.to be_a(URI) }
 
-        it "is expected to point to the correct URL" do
-          expect(strategy_obj.host).to eq "mail.google.com"
-        end
-
-        it "is expected to have sniper params" do
-          expect(strategy_obj.fragment).to start_with("search/")
-        end
+        its(:host)     { is_expected.to eq "mail.google.com" }
+        its(:fragment) { is_expected.to start_with("search/") }
       end
     end
   end
@@ -39,16 +30,19 @@ RSpec.describe SniperLinks do
     context "when supplied an email ending in @outlook.com" do
       let(:email) { "plop@outlook.com" }
 
-      it "is expected to use the Outlook sniper link strategy automatically" do
-        expect(sniper_link_obj.sniper_link_strategy).to be_a(SniperLinks::Strategies::Outlook)
-      end
+      its(:sniper_link_strategy) { is_expected.to be_a(SniperLinks::Strategies::Outlook) }
 
       context "when generating a sniper link" do
         subject(:strategy_obj) { sniper_link_obj.sniper_link(from) }
 
-        it "is expected to generate a sniper link" do
-          expect(strategy_obj).to be_a(URI)
-        end
+        it { is_expected.to be_a(URI) }
+
+        its(:host)  { is_expected.to eq "outlook.live.com" }
+        its(:query) { is_expected.to start_with("login_hint=") }
+      end
+    end
+  end
+
   context "when working with Yahoo" do
     context "when supplied an email ending in @yahoo.com" do
       let(:email) { "plop@yahoo.com" }
